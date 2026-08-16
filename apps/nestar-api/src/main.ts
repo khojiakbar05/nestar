f49017405ab772bf415b683491d119f22eeeb08b
khojiakbar05 <karimjonovkhojiakbar1@gmail.com>
2026-08-16 18:50:01 +0900

    feat: standard integration of logging interceptor

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());   // we can use it anywhere in our project
+	app.useGlobalInterceptors(new LoggingInterceptor())  // logging stanard time before and after
 	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();

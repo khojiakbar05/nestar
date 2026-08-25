262506e77efb71b09d264c8e21838d650029c794
khojiakbar05 <karimjonovkhojiakbar1@gmail.com>
2026-08-25 09:16:47 +0900

    fix: modify view module to record visits

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -3,6 +3,18 @@ import { MemberService } from './member.service';
 import { InternalServerErrorException } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
+<<<<<<< HEAD
+=======
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import * as mongoose from 'mongoose';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
+>>>>>>> 9cd7b3f (fix: modify view module to record visits)
 
 @Resolver()
 export class MemberResolver {
@@ -33,8 +45,62 @@ export class MemberResolver {
 	}
 
 	@Query(() => String)
+<<<<<<< HEAD
 	public async getMember(): Promise<string> {
 		console.log('Mutation: getMember');
 		return this.memberService.getMember();
+=======
+	public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
+		console.log('Query: checkAuthRoles');
+
+		return `hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
+	}
+
+	// Authenticated
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async updateMember(
+		@Args('input') input: MemberUpdate,
+		@AuthMember('_id') memberId: mongoose.Types.ObjectId,
+	): Promise<Member> {
+		console.log('=== UPDATE MEMBER RESOLVER ===');
+		console.log('INPUT:', input);
+		console.log('Mutation: updateMember');
+		console.log('memberId: ', memberId);
+		delete input._id;
+		return this.memberService.updateMember(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query(() => Member)
+	public async getMember(
+		@Args('memberId') input: string,
+		@AuthMember('_id') memberId: mongoose.Types.ObjectId,
+	): Promise<Member> {
+		console.log('Query: getMember');
+		const targetId = shapeIntoMongoObjectId(input);
+		// console.log('memberId: ', memberId);
+
+		return this.memberService.getMember(memberId, targetId);
+	}
+
+	/** ADMIN **/
+
+	// Authorization: ADMIN
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation(() => String)
+	public async getAllMembersByAdmin(@AuthMember() authMember: Member): Promise<string> {
+		console.log('authMember.memberTypr: ', authMember.memberType);
+
+		return this.memberService.getAllMembersByAdmin();
+	}
+
+	// Authorization: ADMIN
+	@Mutation(() => String)
+	public async updateMemberByAdmin(): Promise<string> {
+		console.log('Mutation: updateMemberByAdmin');
+		return this.memberService.updateMemberByAdmin();
+>>>>>>> 9cd7b3f (fix: modify view module to record visits)
 	}
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,17 +1,30 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+<<<<<<< HEAD
 import { Model } from 'mongoose';
+=======
+import { Model, Types } from 'mongoose';
+>>>>>>> 9cd7b3f (fix: modify view module to record visits)
 import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
+<<<<<<< HEAD
+=======
+import { MemberUpdate } from '../../libs/dto/member/member.update';
+import { T } from '../../libs/types/common';
+import { ViewService } from '../view/view.service';
+import { ViewGroup } from '../../libs/enums/view.enum';
+import { ViewInput } from '../../libs/dto/view/view.input';
+>>>>>>> 9cd7b3f (fix: modify view module to record visits)
 
 @Injectable()
 export class MemberService {
 	constructor(
 		@InjectModel('Member') private readonly memberModel: Model<Member>,
 		private authService: AuthService,
+		private viewService: ViewService,
 	) {}
 
 	public async signup(input: MemberInput): Promise<Member> {
@@ -54,6 +67,7 @@ export class MemberService {
 		return response;
 	}
 
+<<<<<<< HEAD
 	public async updateMember(): Promise<string> {
 		console.log('Mutation: updateMember');
 		return 'updateMember executed!';
@@ -62,5 +76,59 @@ export class MemberService {
 	public async getMember(): Promise<string> {
 		console.log('Mutation: getMember');
 		return 'getMember executed!';
+=======
+	public async updateMember(memberId: Types.ObjectId, input: MemberUpdate): Promise<Member> {
+		const result = await this.memberModel
+			.findOneAndUpdate(
+				{
+					_id: memberId,
+					memberStatus: MemberStatus.ACTIVE,
+				},
+				input,
+				{ new: true }, // yangilangan malumotni qaytaradi
+			)
+			.exec();
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+		result.accessToken = await this.authService.createToken(result);
+		return result;
+	}
+
+	public async getMember(memberId: Types.ObjectId, targetId: Types.ObjectId): Promise<Member> {
+		console.log('queryService: getMember');
+		const search: T = {
+			_id: targetId,
+			memberStatus: {
+				$in: [MemberStatus.ACTIVE, MemberStatus.BLOCK],
+			},
+		};
+		const targetMember = await this.memberModel.findOne(search).lean().exec();
+		if (!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		if (memberId) {
+			// record view
+			const viewInput = { memberId: memberId, viewRefId: targetId, viewGroup: ViewGroup.MEMBER };
+			const newView = await this.viewService.recordView(viewInput);
+			if (newView) {
+				
+				// increase member view
+				await this.memberModel.findOneAndUpdate(search, { $inc: { memberViews: 1 } }, { new: true }).exec();
+				targetMember.memberViews++;
+			}
+
+		}
+
+		return targetMember;
+	}
+
+	public async getAllMembersByAdmin(): Promise<string> {
+		console.log('Mutation: getAllMembersByAdmin');
+		return 'getAllMembersByAdmin executed!';
+	}
+
+	public async updateMemberByAdmin(): Promise<string> {
+		console.log('Mutation: updateMemberByAdmin');
+		return 'updateMemberByAdmin executed!';
+>>>>>>> 9cd7b3f (fix: modify view module to record visits)
 	}
 }
